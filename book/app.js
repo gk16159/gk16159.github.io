10b10d8e699f99e22a07bcb76be6caa972431b3e
gk16159 <132127460+gk16159@users.noreply.github.com>
2026-07-12 02:06:50 +0530

    Update app.js

--- book/app.js
+++ book/app.js
@@ -1,1 +1,22 @@
+const book = ePub("book_1.epub");
 
+const rendition = book.renderTo("viewer", {
+    width: "100%",
+    height: "100%",
+    spread: "auto"
+});
+
+rendition.display();
+
+document.getElementById("next").addEventListener("click", () => {
+    rendition.next();
+});
+
+document.getElementById("prev").addEventListener("click", () => {
+    rendition.prev();
+});
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") rendition.next();
+    if (e.key === "ArrowLeft") rendition.prev();
+});
